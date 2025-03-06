0eca133d9005e0ac1fb2a438f560068938fcc1c3
vinnygamer289 <ltronghiep703@gmail.com>
2025-03-06 17:57:32 +0700

    Create addon.js

---
+++ add-on/addon.js
@@ -0,0 +1,13 @@
+let backToTop = document.getElementById("backToTop");
+
+        window.addEventListener("scroll", function () {
+            if (window.scrollY > 300) {
+                backToTop.style.display = "block";
+            } else {
+                backToTop.style.display = "none";
+            }
+        });
+
+        backToTop.addEventListener("click", function () {
+            window.scrollTo({ top: 0, behavior: "smooth" });
+        });
