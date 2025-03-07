55f834311a6d7880a49687e9f4ca26f3edee6a82
vinnygamer289 <ltronghiep703@gmail.com>
2025-03-08 06:33:25 +0700

    Update addon.js

--- add-on/addon.js
+++ add-on/addon.js
@@ -11,3 +11,25 @@ let backToTop = document.getElementById("backToTop");
         backToTop.addEventListener("click", function () {
             window.scrollTo({ top: 0, behavior: "smooth" });
         });
+        document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll("a").forEach(function (link) {
+        link.addEventListener("contextmenu", function (event) {
+            event.preventDefault();
+        });
+        let timeout;
+        link.addEventListener("touchstart", function (event) {
+            timeout = setTimeout(function () {
+                event.preventDefault();
+            }, 500);
+        });
+
+        link.addEventListener("touchend", function () {
+            clearTimeout(timeout);
+        });
+        link.addEventListener("mousedown", function (event) {
+            if (event.button === 2) {
+                event.preventDefault();
+            }
+        });
+    });
+});
